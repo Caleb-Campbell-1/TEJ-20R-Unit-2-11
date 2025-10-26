ff4021f5caa94322f81a24500e8bc10eb80bea37
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2025-10-26 11:43:08 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Campbell
+ * Created on: Oct 2025
+ * This program shows an if...then...else statement
 */
 
-basic.showString('Hello, World!')
+
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+// variables
+let randomNumberTwo = 0
+let randomNumber = 0
+randomNumber = randint(0, 99)
+randomNumberTwo = randint(0, 99)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randomNumber)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randomNumberTwo)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (randomNumber < randomNumberTwo) {
+        basic.showString("1st < 2nd")
+    } else {
+        basic.showString("1st > 2nd")
+    }
+})
